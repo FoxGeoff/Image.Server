028f4761308dcc38d9938248ea9a4b4eb0aea901
FoxGeoff <fox.geoffrey@gmail.com>
2019-05-23 16:13:29 -0700

    Add  Entity.ProductImage and seed data

---
+++ Image.Server/Entities/ProductImage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Image.Server.Entities
+{
+    public class ProductImage
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Date Updated")]
+        public DateTime? DateUpdated { get; set; }
+
+        [MaxLength(50)]
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        [MaxLength(50000)]
+        [Display(Name = "Thumb")]
+        public byte[] ImageThumb { get; set; }
+
+        [MaxLength(150000)]
+        [Display(Name = "Full Image")]
+        public byte[] ImageFull { get; set; }
+
+        public string GetImageThumb()
+        {
+            var base64 = Convert.ToBase64String(ImageThumb);
+
+            return String.Format("data:image/gif;base64,{0}", base64);
+        }
+
+        public string GetImageFull()
+        {
+            var base64 = Convert.ToBase64String(ImageFull);
+
+            return String.Format("data:image/gif;base64,{0}", base64);
+        }
+    }
+}
---
+++ Image.Server/Services/ImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Image.Server.Services
+{
+    public class ImageRepository : IImageRepository
+    {
+
+    }
+}
